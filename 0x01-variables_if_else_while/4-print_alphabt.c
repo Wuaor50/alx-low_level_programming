d8c28a0e4350006fe4cc2b4be6a55636830a2397
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-09 14:22:17 -0700

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print chr a-z except q and e
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = a; ch <= z; ch++)
+	{
+		if (ch == q || ch == e)
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
