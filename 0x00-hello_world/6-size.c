b8fd6c0ea67c4e4855c54bdb2aea81aeca52c283
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-09 02:47:57 -0700

    6 error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,9 @@
 /**
  * main - Entry point
  *
- * Return: Alwas 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int a;
@@ -13,10 +14,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(d));
-	printf("Size of an int: %d byte(s)\n", sizeof(a));
-	printf("Size of a long int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+		printf("Size of a char: %d byte(s)\n", sizeof(d));
+		printf("Size of an int: %d byte(s)\n", sizeof(a));
+		printf("Size of a long int: %d byte(s)\n", sizeof(b));
+		printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+		printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 {	
