fbf04ec5060a795f64f1fc344bf9e4434929838e
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-09 14:49:13 -0700

    5-p commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrption:  prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 on Success
+ */
+int  main(void)
+{
+	int i = 0;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		printf(i);
+
+	}
+	printf('\n');
+	return (o);
+}
