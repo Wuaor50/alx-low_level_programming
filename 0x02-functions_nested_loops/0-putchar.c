1a017a7c90773e9e108a5b7504c82c0ad2fe1011
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-13 12:24:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entrance point
+ *
+ * Descrption:print Putchar
+ * Return/; 0 on success
+ */
+int main(void)
+{
+	char array[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		putchar(array[i]);
+		putchar('\n');
+	}
+	return (0);
+}
