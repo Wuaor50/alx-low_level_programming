4e5ea1d4fcfee12a4e60abfe2689265db8de82d9
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-14 10:18:46 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: 10-add.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/*
+ * add-Add two numbers
+ * @num1: first number to be added
+ * @num2: second number to be added
+ *
+ * Description: Write a function that adds two integers and returns the result.
+ * Return: the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
