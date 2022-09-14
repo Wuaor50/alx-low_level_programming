72d43a03900a6f158d1687556e529f415981aadd
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-14 11:21:42 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,11 @@
-/*
- * File: 10-add.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /*
- * add-Add two numbers
+ * add - A function that adds two integers and returns the result.
  * @num1: first number to be added
  * @num2: second number to be added
- *
- * Description: Write a function that adds two integers and returns the result.
+ * 
+ * Description: A function that adds two integers and returns the result.
  * Return: the result of the addition
  */
 int add(int num1, int num2)
