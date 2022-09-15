ec98ee3a1c7754b9cf46f69398d22c0d4ae5848e
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-15 09:15:17 -0700

    0-main.c, main.h commit

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#define "main.h"
+
+/*
+ * main - test fuction that print if integer is negative or positive
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
