7101abe7201443d0f1f725dc741fd4a0c0e9196a
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-09-16 10:28:14 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,19 +11,18 @@ void print_diagonal(int n)
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
 			{
-				for (space = 0; space < len; space++)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+			}
 
-				_putchar('\\');
+			_putchar('\\');
 
-				if (len == n - 1)
-
-					continue;
-				_putchar('\n');
-																				}
+			if (len == n - 1)
+				continue;
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
