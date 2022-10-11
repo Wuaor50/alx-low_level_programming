e3c70d136f64ed5a3411f3077e62a1ce825179ed
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-10-10 21:50:49 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,19 +18,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
-
 		return (NULL);
-
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
-
 		return (NULL);
-
 	for (nl = 0; name[nl]; nl++)
 		nl++;
 
-	//dog->name = malloc(nl * sizeof(char));
+	dog->name = malloc(nl * sizeof(char));
 
 	if (dog->name == NULL)
 	{
@@ -44,17 +40,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	for (ol = 0; owner[ol]; ol++)
 		ol++;
-	//dog->owner = malloc(ol * sizeof(char));
+	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
-
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < ol; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
-
 }
