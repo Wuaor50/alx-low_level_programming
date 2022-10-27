4a611af0d26ff047c16cd82afecb884089877ff4
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-10-27 14:23:32 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,25 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
+ * print_binary - prints a number as binary string
+ * @n: the number to print
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
+	int bit = sizeof(n) * 8, printed = 0;
 
-	if (n == 0)
+	while (bit)
 	{
-		printf("0");
-		return;
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
 	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	if (!printed)
+		_putchar('0');
 }
